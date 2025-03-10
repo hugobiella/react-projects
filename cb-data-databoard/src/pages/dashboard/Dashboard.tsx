c936c0eb33b550ccaed4b0d6cd7980588b1c9934
hugobiella <hugo.a.biella@hotmail.com>
2025-03-10 16:32:50 -0300

    styling and theming

--- cb-data-databoard/src/pages/dashboard/Dashboard.tsx
+++ cb-data-databoard/src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Box, Paper } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import scss from './Dashboard.module.scss';
 
 const Dashboard = () => {
     return (
-        <h1>Dashboard</h1>
-    )
-}
+        <Box>
+            <Grid container spacing={2} className={scss.topCardsContainer}>
+                <Grid item xs={4}>
+                    <Paper className={scss.dataCard}>xs=4</Paper>
+                </Grid>
+
+                <Grid item xs={4}>
+                    <Paper className={scss.dataCard}>xs=4</Paper>
+                </Grid>
+
+                <Grid item xs={4}>
+                    <Paper className={scss.dataCard}>xs=4</Paper>
+                </Grid>
+            </Grid>
+
+            <Grid container>
+                <Grid item xs={12} marginY={2}>
+                    <Paper className={scss.dataCard}>xs=12</Paper>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+};
 
 export default Dashboard;
--- cb-data-databoard/src/pages/index.tsx
+++ cb-data-databoard/src/pages/index.tsx
@@ -1,31 +1,32 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
 import Dashboard from "@/pages/dashboard";
 import SideMenu from "@/components/SideMenu";
-import Header from "@/components/Header";
 import { useSession } from "next-auth/react";
 import Login from "@/components/Login/Login";
+import scss from "./Home.module.scss";
+import React from "react";
 
-export default function Home() {
+const Home: React.FC = () => {
   const { data: session } = useSession();
   return (
     <>
       <Head>
-        <title>Data Dashboard</title>
+        <title>Dashboard</title>
         <meta name="description" content="Data Dashboard" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <Header />
+      <main className={scss.main}>
         {session && (
           <>
             <SideMenu />
             <Dashboard />
           </>
         )}
-        <Login/>
+        <Login />
       </main>
     </>
   );
-}
+};
+
+export default Home;
