9a5a1c7f03d00d41302891087ff7e227278914cc
hugobiella <hugo.a.biella@hotmail.com>
2025-03-14 12:20:38 -0300

    route guarding

--- cb-data-databoard/src/components/Login/Login.tsx
+++ cb-data-databoard/src/components/Login/Login.tsx
@@ -1,19 +1,23 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Button from "@mui/material/Button";
 
 const Login = () => {
   const { data: session } = useSession();
+
   if (session) {
     return (
       <>
-        <Button variant={'contained'} color={'error'} onClick={() => signOut()}>Sign out</Button>
+        <Button variant={"contained"} color={"error"} onClick={() => signOut()}>
+          Sign out
+        </Button>
       </>
     );
   }
   return (
     <>
-      <h2>Please Log-In</h2> <br />
-      <Button variant={'contained'} color={'success'} onClick={() => signIn()}>Sign in</Button>
+      <Button variant={"contained"} color={"success"} onClick={() => signIn()}>
+        Sign in
+      </Button>
     </>
   );
 };
--- cb-data-databoard/src/pages/index.tsx
+++ cb-data-databoard/src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Dashboard from "@/pages/dashboard";
+import Login from "@/components/Login";
 import { useSession } from "next-auth/react";
-import Login from "@/components/Login/Login";
-import scss from "./Home.module.scss";
+import scss from "../components/Layout/Layout.module.scss";
 import React from "react";
 
 const Home: React.FC = () => {
   const { data: session } = useSession();
+
   return (
     <main className={scss.main}>
       {session && <Dashboard />}
